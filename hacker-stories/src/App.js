9dba3dc756e75a8eb7fe9b6ad1862552436d5d8e
syd333 <sydmiranda@icloud.com>
2022-07-01 13:18:13 -0400

    useReducer

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,20 +1,6 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useReducer } from "react";
 
-//custom hooks
-const useStorageState = (key, initialState) => {
-  const [value, setValue] = React.useState(
-    localStorage.getItem(key) || initialState
-  );
-
-  React.useEffect(() => {
-    localStorage.setItem(key, value);
-  }, [value, key]);
-
-  return [value, setValue];
-};
-
-const App = () => {
-  const stories = [
+  const initialStories = [
     {
       title: "React",
       url: "https://reactjs.org/",
@@ -32,13 +18,107 @@ const App = () => {
       objectID: 1,
     },
   ];
+
+const getAsyncStories = () =>
+  new Promise((resolve) =>
+    setTimeout(() => resolve({ data: { stories: initialStories } }), 2000)
+  );
+
+//custom hooks
+const useStorageState = (key, initialState) => {
+  const [value, setValue] = useState(
+    localStorage.getItem(key) || initialState
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  return [value, setValue];
+};
+
+const storiesReducer = (state, action) => {
+  switch (action.type) {
+    case "STORIES_FETCH_INIT":
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case "STORIES_FETCH_SUCCESS":
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    case "STORIES_FETCH_FAILURE":
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+    case "REMOVE_STORY":
+      return {
+        ...state,
+        data: state.data.filter(
+          (story) => action.payload.objectID !== story.objectID
+        ),
+      };
+    default:
+      throw new Error();
+  }
+};
+const App = () => {
+
   const [searchTerm, setSearchTerm] = useStorageState("search", "React");
+  // const [stories, setStories] = useState(initialStories);
+
+  const [stories, dispatchStories] = useReducer(storiesReducer, {
+    data: [],
+    isLoading: false,
+    isError: false,
+  });
+
+
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [isError, setIsError] = useState(false);
+
+
+
+  useEffect(() => {
+    dispatchStories({ type: "STORIES_FETCH_INIT" });
+    getAsyncStories()
+      .then((result) => {
+        // setStories(result.data.stories);
+        // setIsLoading(false);
+        dispatchStories({
+          type: "STORIES_FETCH_SUCCESS",
+          payload: result.data.stories,
+        });
+        // setIsLoading(false);
+      })
+      .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
+  }, []);
+
+
+
+  const handleRemoveStory = (item) => {
+    // const newStories = stories.filter(
+    //   (story) => item.objectID !== story.objectID
+    // );
+    // setStories(newStories);
+    dispatchStories({
+      type: "REMOVE_STORY",
+      payload: item,
+    });
+  };
 
   const handleSearch = (e) => {
     setSearchTerm(e.target.value);
   };
 
-  const searchedStories = stories.filter((story) =>
+  const searchedStories = stories.data.filter((story) =>
     story.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
@@ -54,8 +134,12 @@ const App = () => {
         <strong>Search:</strong>
       </InputWithLabel>
       <hr />
-
-      <List list={searchedStories} />
+      {stories.isError && <p> Something went wrong ... </p>}
+      {stories.isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+      )}
     </div>
   );
 };
@@ -92,10 +176,10 @@ const InputWithLabel = ({
   );
 };
 
-const List = ({ list }) => (
+const List = ({ list, onRemoveItem }) => (
   <ul>
     {list.map((item) => (
-      <Item key={item.objectID} item={item} />
+      <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
       //       <Item
       //   key={item.objectID}
       //   title={item.title}
@@ -109,18 +193,28 @@ const List = ({ list }) => (
   </ul>
 );
 
-const Item = ({
-  item: { objectID, title, url, author, num_comments, points },
-}) => (
-  // { title, url, author, num_comments, points }
-  <li key={objectID}>
-    <span>
-      <a href={url}>{title}</a>
-    </span>
-    <span>{author}</span>
-    <span>{num_comments}</span>
-    <span>{points}</span>
-  </li>
-);
+const Item = ({ item, onRemoveItem }) => {
+  // { title, url, author, num_comments, points } <-- props coming in culd be like this
+  // or -->  item: { objectID, title, url, author, num_comments, points }, then displayed as {url}{author} etc.
+
+  // const handleRemoveItem = () => {
+  //   onRemoveItem(item);
+  // };
+  return (
+    <li key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+      <span>
+        <button type="button" onClick={() => onRemoveItem(item)}>
+          Dismiss
+        </button>
+      </span>
+    </li>
+  );
+};
 
 export default App;
